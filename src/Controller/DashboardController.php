0d1bed93d0a2515958dab83d0546b099fd25c394
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2023-12-04 16:33:26 +0100

    Added a controller for the dashboard

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class DashboardController extends AbstractController
+{
+    #[Route("/dashboard", name: "app_dashboard")]
+    public function index(Request $request): Response
+    {
+        if (!$request->cookies->get("token")) {
+            //todo
+        } else {
+            return $this->render("dashboard/index.html.twig");
+        }
+    }
+}
