5b2848269f51209aea4504b1b656746eb79892c8
Gaëtan L. H.-F. <81755261+gaetanlhf@users.noreply.github.com>
2024-04-10 15:31:53 +0200

    Update LoginController.php

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\FirebaseFunctions\LoginFunction;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
@@ -17,45 +18,32 @@ class LoginController extends AbstractController
     {
         if ($request->getMethod() === "GET") {
             return $this->render("login/index.html.twig");
-        } else if ($request->getMethod() === "POST") {
-            $token = $request->request->get("token");
-            if (!$this->isCsrfTokenValid("login", $token)) {
-                return $this->redirectToRoute("app_login");
-            }
-
-            $email = $request->request->get("email");
-            $password = $request->request->get("password");
-            $rememberMe = $request->request->get("remember_me");
-            
-            if ($rememberMe == null) {
-                $rememberMe = false;
-            }
-
-            $emailConstraint = new Assert\Email();
-            $emailConstraint->message = "Email address not valid.";
-
-            $errors = $validator->validate(
-                $email,
-                $emailConstraint
-            );
-
-            if (count($errors) > 0) {
-                foreach ($errors as $error) {
-                    $this->addFlash("login_error", $error->getMessage());
-                }
-                return $this->redirectToRoute("app_login");
-            }
-
-            $error = $loginFunction->logIn($email, $password, $rememberMe);
-
-            if (!$error) {
-                return $this->redirectToRoute("app_dashboard");
-            } else {
-                return $this->redirectToRoute("app_login", [
-                    "email" => $email,
-                    "remember_me" => $rememberMe,
-                ]);
-            }    
         }
+
+        $token = $request->request->get("token");
+        if (!$this->isCsrfTokenValid("login", $token)) {
+            return $this->redirectToRoute("app_login");
+        }
+
+        $email = $request->request->get("email");
+        $password = $request->request->get("password");
+        $rememberMe = $request->request->get("remember_me", false);
+
+        $emailConstraint = new Assert\Email(["message" => "Email address not valid."]);
+        $errors = $validator->validate($email, $emailConstraint);
+
+        if (count($errors) > 0) {
+            foreach ($errors as $error) {
+                $this->addFlash("login_error", $error->getMessage());
+            }
+            return $this->redirectToRoute("app_login");
+        }
+
+        $response = $loginFunction->logIn($email, $password, $rememberMe);
+        if ($response instanceof Response) {
+            $response->sendHeaders();
+            return $this->redirectToRoute("app_dashboard");
+        }
+        return $this->redirectToRoute("app_login", ["email" => $email, "remember_me" => $rememberMe]);
     }
 }
